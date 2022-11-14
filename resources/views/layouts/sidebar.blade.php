1b5d2420d9c2c9ecc54c4b7185088596d2bfd242
Sisilia Marcela <102360647+SisiliaMarcela@users.noreply.github.com>
2022-11-14 22:58:15 +0700

    Initial commit

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,36 @@
+<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+    <!-- Add icons to the links using the .nav-icon class
+         with font-awesome or any other icon font library -->
+         <li class="nav-item">
+            <a href='{{ asset("admin/category") }}' class="nav-link">
+              <i class="nav-icon fas fa-eye"></i>
+              <p>
+                Category
+               </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href='{{ asset("admin/user-role") }}' class="nav-link">
+              <i class="nav-icon fas fa-eye"></i>
+              <p>
+                User Role
+               </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href='{{ asset("admin/reservation-status") }}' class="nav-link">
+              <i class="nav-icon fas fa-eye"></i>
+              <p>
+                Reservation Status
+               </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href='{{ asset("admin/missing-item-status") }}' class="nav-link">
+              <i class="nav-icon fas fa-eye"></i>
+              <p>
+                Missing Item Status
+               </p>
+            </a>
+          </li>
+</ul>
